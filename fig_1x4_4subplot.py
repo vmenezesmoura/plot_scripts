3645af3ed490014e210c01a767eb9737ad0d0cd8
Vitor Moura <vmenezesmoura@gmail.com>
2024-09-26 20:41:11 -0300

    Corrections in script
    Previous script was a version zero, with a bunch of error. Now it works fine

--- fig_1x4_4subplot.py
+++ fig_1x4_4subplot.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle: str, figsize: tuple[int] = (8, 4), width_ratios: list[float] = [2, 1.5, 1, 1]):
+def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle: str, figsize: tuple[int] = (14, 4), width_ratios: list[float] = [2, 1.5, 1, 1]):
     '''
     Plots a 1x4 grid of subplots comparing two series (observed vs. predicted) with various statistical representations.
 
@@ -23,8 +23,8 @@ def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle
     --------
     fig : matplotlib.figure.Figure
         The resulting figure object.
-    df_s : pd.DataFrame
-        A DataFrame containing the observed and predicted data.
+    df_stats : pd.DataFrame
+        A DataFrame containing the statistics of observed and predicted data.
 
     Description:
     ------------
@@ -44,7 +44,6 @@ def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle
     import numpy as np
     import matplotlib.pyplot as plt
     from matplotlib.gridspec import GridSpec
-    from sklearn.linear_model import LinearRegression
     from sklearn.metrics import r2_score
     from sklearn.metrics import mean_squared_error
 
@@ -55,9 +54,9 @@ def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle
     color_iqr = ['mistyrose', 'lavender']  # Colors for interquartile range areas
     color_linestat = {
         'mean': ['darkred', 'darkblue'],  # Colors for mean lines
-        'median': ['black', 'white'],     # Colors for median lines
-        'R2': 'white',                    # Color for R² line in the scatter plot
-        'nbias': 'white'                  # Color for normalized BIAS line in error plot
+        'median': ['yellow', 'green'],     # Colors for median lines
+        'R2': 'lightgray',                    # Color for R² line in the scatter plot
+        'nbias': 'lightgray'                  # Color for normalized BIAS line in error plot
     }
     
     # Axis labels for the subplots
@@ -69,31 +68,21 @@ def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle
     xlabel_subplot3 = ''
     ylabel_subplot3 = xlabel_subplot0
     
-    # Create lists of x and y labels for the subplots using the defined label variables
-    xlabel = [locals()[f'xlabel_subplot{i}'] for i in range(fig_grade[1])]
-    ylabel = [locals()[f'ylabel_subplot{i}'] for i in range(fig_grade[1])]
-    
     # List of statistical measures to calculate
     list_stats = ['mean', 'range', 'std', 'iqr', 'skew', 'kurtosis']
 
     # Prepare the data by finding the common index and concatenating the series into a DataFrame
     common_index = s0.index.intersection(s.index)
-    df_s = pd.concat([s0, s], index=1)
     max_value = max(s0.max(), s.max())
     min_value = min(s0.min(), s.min())
-    max_abs_error = (s - s0).abs().max
+    s0 = s0.loc[common_index]
+    s = s.loc[common_index]
     
     # Define histogram bins for data and error distributions
-    bins_minamax = range(min_value, max_value * 1.1, (max_value - min_value) / 30)
-    bins_absmaxamax = range(-max_abs_error, max_abs_error * 1.1, max_abs_error / 15)
-    
-    # Perform linear regression to find the slope and intercept of the line fitting the observed vs. predicted data
-    slope, intercept = np.polyfit(s0, s, 1)
+    bins_minamax = np.arange(min_value, max_value * 1.1, (max_value - min_value) / 30)
 
     # Calculate statistical measures for observed and predicted data
     for k in ['0', '']:
-        locals()[f's{k}_'] = locals()[f's{k}']
-        locals()[f's{k}'] = locals()[f's{k}_'].loc[common_index]
         locals()[f'{list_stats[0]}_s{k}'] = locals()[f's{k}'].mean()
         locals()[f'{list_stats[1]}_s{k}'] = locals()[f's{k}'].max() - locals()[f's{k}'].min()
         locals()[f'{list_stats[2]}_s{k}'] = locals()[f's{k}'].std()
@@ -101,6 +90,12 @@ def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle
         locals()[f'{list_stats[4]}_s{k}'] = locals()[f's{k}'].skew()
         locals()[f'{list_stats[5]}_s{k}'] = locals()[f's{k}'].kurtosis()
     
+    # Perform linear regression to find the slope and intercept of the line fitting the observed vs. predicted data
+    df_s = pd.concat([s0, s], axis=1)
+    max_abs_error = (s - s0).abs().max()
+    bins_absmaxamax = np.arange(-max_abs_error, max_abs_error * 1.1, max_abs_error / 15)
+    slope, intercept = np.polyfit(s0, s, 1)
+
     # Create Series to store calculated statistics for both observed and predicted data
     stats_s0 = pd.Series(
         [
@@ -128,13 +123,13 @@ def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle
     )
     
     # Concatenate statistics into a DataFrame for display
-    df_stats = pd.concat([stats_s0, stats_s], index=1)
+    df_stats = pd.concat([stats_s0, stats_s], axis=1)
 
     # Calculate correlation, R², normalized RMSE, and normalized BIAS between observed and predicted data
     corr = df_s.corr().iloc[0,1]
-    r2 = r2_score(s, LinearRegression().fit(df_s[s0.name], s).predict(df_s[s0.name]))
-    nrmse =   np.sqrt(mean_squared_error(s0, s)) / locals()['range_s0'] if locals()['range_s0'] != 0 else np.nan
-    nbias =   (s - s0).mean() / locals()['range_s0'] if locals()['range_s0'] != 0 else np.nan
+    r2 = r2_score(s0, s)
+    nrmse =   np.sqrt(mean_squared_error(s0, s)) / stats_s0['range'] if stats_s0['range'] != 0 else np.nan
+    nbias =   (s - s0).mean() / stats_s0['range'] if stats_s0['range'] != 0 else np.nan
 
     # Create the figure and subplots using GridSpec
     fig = plt.figure(figsize=figsize)
@@ -162,37 +157,58 @@ def fig_1x4_4subplot(s0: pd.Series, s: pd.Series, xlabel_subplot0: str, suptitle
     axes[3].set_ylabel(ylabel_subplot3)
 
     # Plot data on the first subplot: histograms with mean and median lines and IQR areas
-    axes[0].axvspan(s0.quantile(0.25), s0.quantile(0.75), color=color_iqr[0], label=f'IQR {xlabel_subplot1}', alpha=0.3)
-    axes[0].axvspan(s.quantile(0.25), s.quantile(0.75), color=color_iqr[1], label=f'IQR {ylabel_subplot1}', alpha=0.3)
-    axes[0].axvline(x=s0.median(), color=color_linestat['median'][0], ls='--', lw=1, label=f'Mediana {xlabel_subplot1}')
-    axes[0].axvline(x=s.median(), color=color_linestat['median'][1], ls='--', lw=1, label=f'Mediana {ylabel_subplot1}')
-    axes[0].axvline(x=s0.mean(), color=color_linestat['mean'][0], lw=1, label=f'Média {xlabel_subplot1}')
-    axes[0].axvline(x=s.mean(), color=color_linestat['mean'][1], lw=1, label=f'Média {ylabel_subplot1}')
-    df_s.hist(density=True, histtype='bar', bins=bins_minamax, ax=axes[0], color=color_data, label=[xlabel_subplot1, ylabel_subplot1])
-    axes[0].legend(loc='best', fontsize='x-small')
+    axes[0].axvspan(s0.quantile(0.25), s0.quantile(0.75), color=color_iqr[0], label=f'IQR {xlabel_subplot1[:3]}')
+    axes[0].axvspan(s.quantile(0.25), s.quantile(0.75), color=color_iqr[1], label=f'IQR {ylabel_subplot1[:4]}')
+    axes[0].axvline(x=s0.median(), color=color_linestat['median'][0], ls='-', lw=1, label=f'mediana {xlabel_subplot1[:3]}={s0.median():.2f}')
+    axes[0].axvline(x=s.median(), color=color_linestat['median'][1], ls='-', lw=1, label=f'mediana {ylabel_subplot1[:4]}={s.median():.2f}')
+    axes[0].hist(s0, density=True, bins=bins_minamax, color=color_data[0], label=xlabel_subplot1, alpha=0.5)
+    axes[0].hist(s, density=True, bins=bins_minamax, color=color_data[1], label=ylabel_subplot1, alpha=0.5)
+    axes[0].axvline(x=stats_s0['mean'], color=color_linestat['mean'][0], ls=':', label=f'média {xlabel_subplot1[:3]}={stats_s0['mean']:.2f}')
+    axes[0].axvline(x=stats_s['mean'], color=color_linestat['mean'][1], ls=':', label=f'média {ylabel_subplot1[:4]}={stats_s['mean']:.2f}')
 
     # Plot data on the second subplot: scatter plot with linear fit and R² annotation
-    axes[1].scatter(s0, s, s=5, color=color_comparison, label=f'r$^2$={r2:.2f}')
-    axes[1].plot(s0, s0 * slope + intercept, lw=1, ls='--', color=color_linestat['R2'], label=f'corr={corr:.2f}, r$^2$={r2:.2f}')
-    axes[1].plot([min_value, max_value], [min_value, max_value], lw=1, color='gray', ls=':', label=f'{xlabel_subplot1}={ylabel_subplot1}')
-    axes[1].legend(loc='best', fontsize='x-small')
+    axes[1].scatter(s0, s, s=1, color=color_comparison, label='dados cruzados', alpha=0.3)
+    axes[1].plot([min_value, max_value], [min_value, max_value], lw=1, color='black', ls=':', label='identidade')
+    axes[1].plot(s0, slope * s0 + intercept, lw=1, ls='--', color=color_linestat['R2'], label=f'ajuste R$^2$={r2:.2f}\n[Corr={corr:.2f}]')
 
     # Plot data on the third subplot: error histogram with median error and normalized BIAS annotation
-    (s - s0).hist(density=True, histtype='bar', bins=bins_absmaxamax, ax=axes[2], color=color_comparison, label=f'{xlabel_subplot1}-{ylabel_subplot1}')
-    axes[2].axvline(x=(s - s0).median(), color=color_linestat['median'][0], ls='--', lw=1, label=f'Mediana do erro')
-    axes[2].axvline(x=nbias * (locals()['range_s0']), color=color_linestat['nbias'], lw=1, label=f'nBIAS={nbias:.2f}')
-    axes[2].legend(loc='best', fontsize='x-small')
+    axes[2].hist(s - s0, density=True, bins=bins_absmaxamax, color=color_comparison, label='erro da previsão', alpha=0.7)
+    axes[2].axvline(x=nbias * stats_s0['range'], color=color_linestat['nbias'], ls='--', lw=1, label=f'nBIAS={nbias:.2f}\n[BIAS={nbias * stats_s0['range']:.2f}]\n[nRMSE={nrmse:.2f}]')
 
     # Plot data on the fourth subplot: boxplots for observed and predicted data
-    df_s.plot(kind='box', vert=1, widths=0.5, grid=0, ax=axes[3], color=dict(boxes=color_data, whiskers=color_data, medians=color_linestat['median'], caps=color_data))
+    box = axes[3].boxplot(df_s, patch_artist=True, widths=0.5, labels=[xlabel_subplot1[:3], ylabel_subplot1[:4]])
+
+    # Aplicando estilos para cada boxplot individualmente
+    for m, (patch, color) in enumerate(zip(box['boxes'], color_data)):
+        patch.set_facecolor(color_iqr[m])
+        patch.set_edgecolor(color)
+        patch.set_linewidth(1.5)  # Espessura das bordas da caixa
+        
+        # Aplicando o mesmo estilo para as outras partes do boxplot
+        for n in range(m * 2, m * 2 + 2):
+            box['whiskers'][n].set_color(color)
+            box['whiskers'][n].set_linewidth(1.5)  # Espessura dos whiskers
+        
+        for n in range(m * 2, m * 2 + 2):
+            box['caps'][n].set_color(color_linestat['mean'][m])
+            box['caps'][n].set_linewidth(1.5)  # Espessura dos caps
+
+        # Aplicando o estilo na linha da mediana com maior espessura
+        box['medians'][m].set_color(color_linestat['median'][m])
+        box['medians'][m].set_linewidth(3)  # Espessura maior para a mediana
+        
+        # Estilo dos outliers
+        for flier in box['fliers']:
+            flier.set(marker='.', color=color_linestat['mean'][m], markersize=4, alpha=0.7)
 
     # Add grid and legend to subplots
-    for ax in axes:
-        ax.grid(axis='both')
-        ax.legend()
+    for i in range(len(axes)):
+        axes[i].grid(axis='both')
+        if i%fig_grade[1] != fig_grade[1] - 1:
+            axes[i].legend(fontsize=7)
 
     # Add main title and adjust layout for better display
     fig.suptitle(suptitle)
     fig.tight_layout(rect=[0, 0, 1, 0.95])
     
-    return fig, df_s
+    return fig, df_stats
--- fig_3x4_12subplot.py
+++ fig_3x4_12subplot.py
@@ -1,24 +1,68 @@
 import pandas as pd
 
-def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:list[str], title:list[str], suptitle: str, figsize: tuple[int] = (8, 10), width_ratios: list[float] = [2, 1.5, 1, 1]):
+def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:list[str], title:list[str], suptitle: str, figsize: tuple[int] = (14, 12), width_ratios: list[float] = [2, 1.5, 1, 1], height_ratios: list[float] = [1, 1, 1.5]):
+
+    '''
+    Plots a 3x4 grid of subplots comparing two series (observed vs. predicted) with various statistical representations.
+
+    Parameters:
+    -----------
+    s0 : list[pd.Series]
+        The observed listed data series.
+    s : list[pd.Series]
+        The predicted listed data series.
+    xlabel_subplot0 : list[str]
+        Label list for the x-axis of the firsts subplot.
+    title : list[str]
+        Label list for the title of the firsts subplot.
+    suptitle : str
+        The main title for the figure.
+    figsize : tuple[int], optional
+        Size of the figure (width, height), default is (8, 4).
+    width_ratios : list[float], optional
+        Width ratios for the subplots, default is [2, 1.5, 1, 1].
+
+    Returns:
+    --------
+    fig : matplotlib.figure.Figure
+        The resulting figure object.
+    df_concatenado : pd.DataFrame
+        A DataFrame containing the statistics of observed and predicted for the three series of listed data.
+
+    Description:
+    ------------
+    This function creates a figure with 12 subplots arranged in a 3x4 grid:
+    1. Histogram comparison of the observed vs. predicted data with statistics (mean, median).
+    2. Scatter plot comparing observed vs. predicted values along with a linear fit and R² value.
+    3. Histogram of the prediction errors, showing the median error and normalized BIAS.
+    4. Boxplots showing the distribution of observed and predicted values.
+
+    Additional statistics (mean, range, standard deviation, interquartile range, skewness, and kurtosis) are calculated for each series and displayed as text or lines in the respective plots.
+
+    The function also performs linear regression to compute the slope and intercept for the scatter plot comparison.
+
+    Colors, titles, and labels are configured for each subplot. The layout is tightened for better visualization, and the overall figure title is set to the provided 'suptitle'.
+    '''
 
     import numpy as np
     import matplotlib.pyplot as plt
     from matplotlib.gridspec import GridSpec
-    from sklearn.linear_model import LinearRegression
     from sklearn.metrics import r2_score
     from sklearn.metrics import mean_squared_error
 
+    # Set up the figure grid and color configurations
     fig_grade = (3, 4)
-    color_data = ['red', 'blue']
-    color_comparison = 'purple'
-    color_iqr = ['mistyrose', 'lavender']
+    color_data = ['red', 'blue']  # Colors for observed and predicted data
+    color_comparison = 'purple'   # Color for comparison elements (scatter plot and error)
+    color_iqr = ['mistyrose', 'lavender']  # Colors for interquartile range areas
     color_linestat = {
-        'mean':['darkred', 'darkblue'],
-        'median':['black', 'white'],
-        'R2':'white',
-        'nbias':'white'
+        'mean': ['darkred', 'darkblue'],  # Colors for mean lines
+        'median': ['yellow', 'green'],     # Colors for median lines
+        'R2': 'lightgray',                    # Color for R² line in the scatter plot
+        'nbias': 'lightgray'                  # Color for normalized BIAS line in error plot
     }
+
+    # Axis labels for the subplots
     ylabel_subplot0 = 'densidade normalizada'
     xlabel_subplot1 = 'obsevado'
     ylabel_subplot1 = 'previsto'
@@ -26,12 +70,15 @@ def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:
     ylabel_subplot2 = ylabel_subplot0
     xlabel_subplot3 = ''
     ylabel_subplot3 = xlabel_subplot0
-    xlabel = [locals()[f'xlabel_subplot{i}'] for i in range(fig_grade[1])]
-    ylabel = [locals()[f'ylabel_subplot{i}'] for i in range(fig_grade[1])]
+
+    # List of statistical measures to calculate
     list_stats = ['mean', 'range', 'std', 'iqr', 'skew', 'kurtosis']
 
+    # Checks out the size of listed series given
     if len(ss0) != len(ss):
         raise ValueError('ss0 tem quantidade de séries para comparação diferente de ss')
+    
+    # Adjusts listed xlabels and titles to full len of 3
     while len(xlabel_subplot0) < len(ss0):
         if len(xlabel_subplot0) == 1:
             xlabel_subplot0 = xlabel_subplot0 * 3
@@ -43,6 +90,7 @@ def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:
         else:
             title.append('')
 
+    # Create lists of variables to incorporate calculated stats
     df_ss = []
     max_values = []
     min_values = []
@@ -57,21 +105,23 @@ def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:
     r2s = []
     nrmses = []
     nbiass = []
-    for i in range(len(ss0)):
+
+    for i in range(len(ss0)): # Loop through index of listed series of observed data
         s0 = ss0[i]
         s = ss[i]
+
+        # Prepare the data by finding the common index and concatenating the series into a DataFrame
         common_index = s0.index.intersection(s.index)
-        df_s = pd.concat([s0, s], index=1)
         max_value = max(s0.max(), s.max())
         min_value = min(s0.min(), s.min())
-        max_abs_error = (s - s0).abs().max
-        bins_minamax = range(min_value, max_value * 1.1, (max_value - min_value) / 30)
-        bins_absmaxamax = range(-max_abs_error, max_abs_error * 1.1, max_abs_error / 15)
-        slope, intercept = np.polyfit(s0, s, 1)
+        s0 = s0.loc[common_index]
+        s = s.loc[common_index]
 
+        # Define histogram bins for data and error distributions
+        bins_minamax = np.arange(min_value, max_value * 1.1, (max_value - min_value) / 30)
+
+        # Calculate statistical measures for observed and predicted data
         for k in ['0', '']:
-            locals()[f's{k}_'] = locals()[f's{k}']
-            locals()[f's{k}'] = locals()[f's{k}_'].loc[common_index]
             locals()[f'{list_stats[0]}_s{k}'] = locals()[f's{k}'].mean()
             locals()[f'{list_stats[1]}_s{k}'] = locals()[f's{k}'].max() - locals()[f's{k}'].min()
             locals()[f'{list_stats[2]}_s{k}'] = locals()[f's{k}'].std()
@@ -79,6 +129,13 @@ def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:
             locals()[f'{list_stats[4]}_s{k}'] = locals()[f's{k}'].skew()
             locals()[f'{list_stats[5]}_s{k}'] = locals()[f's{k}'].kurtosis()
         
+        # Perform linear regression to find the slope and intercept of the line fitting the observed vs. predicted data
+        df_s = pd.concat([s0, s], axis=1)
+        max_abs_error = (s - s0).abs().max()
+        bins_absmaxamax = np.arange(-max_abs_error, max_abs_error * 1.1, max_abs_error / 15)
+        slope, intercept = np.polyfit(s0, s, 1)
+
+        # Create Series to store calculated statistics for both observed and predicted data
         stats_s0 = pd.Series(
             [
             locals()[f'{list_stats[0]}_s0'],
@@ -103,13 +160,17 @@ def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:
             index=list_stats, 
             name=ylabel_subplot1
         )
-        df_stats = pd.concat([stats_s0, stats_s], index=1)
 
+        # Concatenate statistics into a DataFrame for display
+        df_stats = pd.concat([stats_s0, stats_s], axis=1)
+
+        # Calculate correlation, R², normalized RMSE, and normalized BIAS between observed and predicted data
         corr = df_s.corr().iloc[0,1]
-        r2 = r2_score(s, LinearRegression().fit(df_s[s0.name], s).predict(df_s[s0.name]))
+        r2 = r2_score(s0, s)
         nrmse =   np.sqrt(mean_squared_error(s0, s)) / locals()['range_s0'] if locals()['range_s0'] != 0 else np.nan
         nbias =   (s - s0).mean() / locals()['range_s0'] if locals()['range_s0'] != 0 else np.nan
 
+        # Append calculated stats to lists
         df_ss.append(df_s)
         max_values.append(max_value)
         min_values.append(min_value)
@@ -126,8 +187,9 @@ def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:
         nrmses.append(nrmse)
         nbiass.append(nbias)
 
+        # Create the figure and subplots using GridSpec
     fig = plt.figure(figsize=figsize)
-    gs = GridSpec(fig_grade[0], fig_grade[1], width_ratios=width_ratios)
+    gs = GridSpec(fig_grade[0], fig_grade[1], width_ratios=width_ratios, height_ratios=height_ratios)
     axes = []
 
     for i in range(fig_grade[0]):
@@ -135,54 +197,87 @@ def fig_3x4_4subplot(ss0: list[pd.Series], ss: list[pd.Series], xlabel_subplot0:
         for j in range(fig_grade[1]):
             axes[i].append(fig.add_subplot(gs[i, j]))
 
+    # Loop through each row in figure
     for i in range(fig_grade[0]):
         s0 = ss0[i]
         s = ss[i]
+        common_index = s0.index.intersection(s.index)
+        s0 = s0.loc[common_index]
+        s = s.loc[common_index]
 
+        # Configure each subplot with appropriate titles and labels
         axes[i][0].set_title(f'Distribuição {xlabel_subplot1} vs {ylabel_subplot1} [{title[i]}]')
         axes[i][0].set_xlabel(xlabel_subplot0[i])
         axes[i][0].set_ylabel(ylabel_subplot0)
 
-        axes[i][1].set_title(f'Correlação')
+        axes[i][1].set_title(f'Correlação [{title[i]}]')
         axes[i][1].set_xlabel(xlabel_subplot1)
         axes[i][1].set_ylabel(ylabel_subplot1)
 
-        axes[i][2].set_title(f'Erro')
+        axes[i][2].set_title(f'Erro [{title[i]}]')
         axes[i][2].set_xlabel(xlabel_subplot2)
         axes[i][2].set_ylabel(ylabel_subplot2)
 
-        axes[i][3].set_title(f'Boxplot')
+        axes[i][3].set_title(f'Boxplot [{title[i]}]')
         axes[i][3].set_xlabel(xlabel_subplot3)
         axes[i][3].set_ylabel(ylabel_subplot3)
 
-        axes[i][0].axvspan(s0.quantile(0.25), s0.quantile(0.75), color=color_iqr[0], label=f'IQR {xlabel_subplot1}', alpha=0.3)
-        axes[i][0].axvspan(s.quantile(0.25), s.quantile(0.75), color=color_iqr[1], label=f'IQR {ylabel_subplot1}', alpha=0.3)
-        axes[i][0].axvline(x=s0.median(), color=color_linestat['median'][0], linestyle=':', label=f'mediana {xlabel_subplot1} [{s0.median():.2f}]')
-        axes[i][0].axvline(x=s.median(), color=color_linestat['median'][1], linestyle=':', label=f'mediana {ylabel_subplot1} [{s.median():.2f}]')
+        # Plot data on the first subplot: histograms with mean and median lines and IQR areas
+        axes[i][0].axvspan(s0.quantile(0.25), s0.quantile(0.75), color=color_iqr[0], label=f'IQR {xlabel_subplot1[:3]}')
+        axes[i][0].axvspan(s.quantile(0.25), s.quantile(0.75), color=color_iqr[1], label=f'IQR {ylabel_subplot1[:4]}')
+        axes[i][0].axvline(x=s0.median(), color=color_linestat['median'][0], ls='-', lw=1, label=f'mediana {xlabel_subplot1[:3]} [{s0.median():.2f}]')
+        axes[i][0].axvline(x=s.median(), color=color_linestat['median'][1], ls='-', lw=1, label=f'mediana {ylabel_subplot1[:4]} [{s.median():.2f}]')
         axes[i][0].hist(s0, density=True, bins=bins_minamaxs[i], color=color_data[0], label=xlabel_subplot1, alpha=0.5)
         axes[i][0].hist(s, density=True, bins=bins_minamaxs[i], color=color_data[1], label=ylabel_subplot1, alpha=0.5)
-        axes[i][0].axvline(x=stats_s0s[i]['mean'], color=color_linestat['mean'][0], linestyle='--', label=f'média {stats_s0s[i].name} [{stats_s0s[i]['mean']:.2f}]')
-        axes[i][0].axvline(x=stats_ss[i]['mean'], color=color_linestat['mean'][1], linestyle='--', label=f'média {stats_ss[i].name} [{stats_ss[i]['mean']:.2f}]')
+        axes[i][0].axvline(x=stats_s0s[i]['mean'], color=color_linestat['mean'][0], ls=':', label=f'média {xlabel_subplot1[:3]} [{stats_s0s[i]['mean']:.2f}]')
+        axes[i][0].axvline(x=stats_ss[i]['mean'], color=color_linestat['mean'][1], ls=':', label=f'média {ylabel_subplot1[:4]} [{stats_ss[i]['mean']:.2f}]')
 
-        axes[i][1].scatter(s0.values, s.values, color=color_comparison, marker='o', s=3, label='dados cruzados')
-        axes[i][1].plot([0, 1], [0, 1], color='black', linestyle='-', linewidth=1, label=f'identidade')
-        axes[i][1].plot(s0, slopes[i] * s0 + intercepts[i], color=color_linestat['R2'], linestyle='--', linewidth=2, label=f'ajuste R² [{r2s[i]:.2f}]')
+        # Plot data on the second subplot: scatter plot with linear fit and R² annotation
+        axes[i][1].scatter(s0, s, s=1, color=color_comparison, label='dados cruzados', alpha=0.3)
+        axes[i][1].plot([min_values[i], max_values[i]], [min_values[i], max_values[i]], lw=1, color='black', ls=':', label='identidade')
+        axes[i][1].plot(s0, slopes[i] * s0 + intercepts[i], lw=1, ls='--', color=color_linestat['R2'], label=f'ajuste R$^2$={r2s[i]:.2f}\n[Corr={corrs[i]:.2f}]')
 
-        axes[i][2].axvspan((s - s0).quantile(0.25), (s - s0).quantile(0.75), color='gray', label=f'IQR erro', alpha=0.3)
-        axes[i][2].axvline(x=(s - s0).median(), color='black', linestyle=':', label=f'mediana erro [{(s - s0).median():.2f}]')
-        axes[i][2].hist(s - s0, bins=bins_absmaxamaxs[i], color=color_comparison, label='erro dos dados')
-        axes[i][2].axvline(x=nbiass[i], color=color_linestat['nbias'], linestyle='--', label=f'BIAS normalizado [{nbiass[i]:.2f}]')
+        # Plot data on the third subplot: error histogram with median error and normalized BIAS annotation
+        axes[i][2].hist(s - s0, density=True, bins=bins_absmaxamaxs[i], color=color_comparison, label='erro da previsão', alpha=0.7)
+        axes[i][2].axvline(x=nbiass[i] * stats_s0s[i]['range'], color=color_linestat['nbias'], linestyle='--', label=f'nBIAS={nbiass[i]:.2f}\n[BIAS={nbiass[i] * stats_s0s[i]['range']:.2f}]\n[nRMSE={nrmses[i]:.2f}]')
 
-        df_ss[i].plot(kind='box', vert=1, widths=0.5, grid=0, ax=axes[i][3], color=dict(boxes=color_data, whiskers=color_data, medians=color_linestat['median'], caps=color_data))
+        # Plot data on the fourth subplot: boxplots for observed and predicted data
+        box = axes[i][3].boxplot(df_ss[i], patch_artist=True, widths=0.5, labels=[xlabel_subplot1[:3], ylabel_subplot1[:4]])
 
-        for ax in axes[i]:
-            ax.grid(axis='both')
-            ax.legend()
+        # Aplicando estilos para cada boxplot individualmente
+        for m, (patch, color) in enumerate(zip(box['boxes'], color_data)):
+            patch.set_facecolor(color_iqr[m])
+            patch.set_edgecolor(color)
+            patch.set_linewidth(1.5)  # Espessura das bordas da caixa
+            
+            # Aplicando o mesmo estilo para as outras partes do boxplot
+            for n in range(m * 2, m * 2 + 2):
+                box['whiskers'][n].set_color(color)
+                box['whiskers'][n].set_linewidth(1.5)  # Espessura dos whiskers
+            
+            for n in range(m * 2, m * 2 + 2):
+                box['caps'][n].set_color(color_linestat['mean'][m])
+                box['caps'][n].set_linewidth(1.5)  # Espessura dos caps
 
-   
+            # Aplicando o estilo na linha da mediana com maior espessura
+            box['medians'][m].set_color(color_linestat['median'][m])
+            box['medians'][m].set_linewidth(3)  # Espessura maior para a mediana
+            
+            # Estilo dos outliers
+            for flier in box['fliers']:
+                flier.set(marker='.', color=color_linestat['mean'][m], markersize=4, alpha=0.7)
+
+        # Add grid and legend to subplots
+        for j in range(len(axes[i])):
+            axes[i][j].grid(axis='both')
+            if j%fig_grade[1] != fig_grade[1] - 1:
+                axes[i][j].legend(fontsize=7)
+
+    # Add main title and adjust layout for better display
     fig.suptitle(suptitle)
-    fig.tight_layout()    
+    fig.tight_layout(rect=[0, 0, 1, 0.95])    
 
-    df_concatenado = pd.concat([df.add_suffix(sufixo) for df, sufixo in zip(df_ss, ['.1', '.2', '.3'])], axis=1)
+    # Concatenate df_stats to a single DataFrame
+    df_concatenado = pd.concat([df.add_suffix('.' + sufixo) for df, sufixo in zip(df_statss, title)], axis=1)
 
     return fig, df_concatenado
